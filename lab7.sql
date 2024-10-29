d941705f49e8eb8c54a4076e50e8a0c7e08747f2
HVoldymyr <165041843+HVoldymyr@users.noreply.github.com>
2024-10-29 15:03:02 +0100

    Update lab7.sql

--- lab7.sql
+++ lab7.sql
@@ -7,45 +7,47 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
-DROP TABLE IF EXISTS `articles`;
-CREATE TABLE `articles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  `category_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE `categories` (
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments` (
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `article_id` int(11) NOT NULL,
-  `user_ip` varchar(45) NOT NULL,
-  `comment` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `post_id` int(11) NOT NULL,
+  `responsed_id` int(11) DEFAULT NULL,
+  `user_name` varchar(100) DEFAULT NULL,
+  `comment` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-DROP TABLE IF EXISTS `ratings`;
-CREATE TABLE `ratings` (
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `article_id` int(11) NOT NULL,
-  `user_ip` varchar(45) NOT NULL,
-  `rating` tinyint(4) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `category_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `author_name` varchar(100) DEFAULT NULL,
+  `watched_amount` int(11) DEFAULT NULL,
+  `likes` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
--- 2024-10-27 16:59:45
+DROP TABLE IF EXISTS `rating`;
+CREATE TABLE `rating` (
+  `post_id` int(11) NOT NULL,
+  `user_ip` varchar(50) NOT NULL,
+  `rating` int(11) NOT NULL,
+  UNIQUE KEY `post_id_user_ip` (`post_id`,`user_ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- 2024-10-29 14:02:26
